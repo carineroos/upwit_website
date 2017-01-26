622e16e109398353ce3e9ab875aba558e894e6e1
Vanessa Me Tonini <vanessametonini@gmail.com>
2017-01-25 22:17:18 -0200

    Iniciado estilos e scripts

---
+++ config.rb
@@ -0,0 +1,10 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+fonts_dir = "fonts/"
+javascripts_dir = "js"
+relative_assets = true
+
+output_style = :expanded
+environment = :development
---
+++ js/upwit.js
@@ -0,0 +1,33 @@
+
+var banners = ["img/banners/citiesforeveryone_home_1200.jpg", "img/banners/prt_1920x638_1484826000.jpg"];
+var bannerAtual = 0;
+
+function trocaBanner() {
+	bannerAtual = (bannerAtual + 1) % 2;
+	document.querySelector('.banner img').src = banners[bannerAtual];
+}
+
+var timer = setInterval(trocaBanner, 4000);
+var controle = document.querySelector('.pause');
+
+controle.onclick = function() {
+	if (controle.className == 'pause') {
+	  clearInterval(timer);
+	  controle.className = 'play';
+	} else {
+	  timer = setInterval(trocaBanner, 4000);
+	  controle.className = 'pause';
+	}
+};
+
+var botaoMenu = document.querySelector('.nav-btn');
+var navLinks = document.querySelector('.nav-links');
+
+botaoMenu.onclick = function(){
+
+	if (navLinks.style.display == 'block') {
+		navLinks.style.display = 'none';
+	} else {
+		navLinks.style.display = 'block';
+	}
+}
