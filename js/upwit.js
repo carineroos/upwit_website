112d4f5b542ad86822a28b5c1aa5a588fe76776e
Vanessa Me Tonini <vanessametonini@gmail.com>
2017-01-25 22:32:17 -0200

    Ajustes na pagina smart-cities e no script do banner

--- js/upwit.js
+++ js/upwit.js
@@ -1,24 +1,26 @@
 
-var banners = ["img/banners/citiesforeveryone_home_1200.jpg", "img/banners/prt_1920x638_1484826000.jpg"];
-var bannerAtual = 0;
+if(document.querySelector('#home')){
+	var banners = ["img/banners/citiesforeveryone_home_1200.jpg", "img/banners/prt_1920x638_1484826000.jpg"];
+	var bannerAtual = 0;
 
-function trocaBanner() {
-	bannerAtual = (bannerAtual + 1) % 2;
-	document.querySelector('.banner img').src = banners[bannerAtual];
-}
-
-var timer = setInterval(trocaBanner, 4000);
-var controle = document.querySelector('.pause');
-
-controle.onclick = function() {
-	if (controle.className == 'pause') {
-	  clearInterval(timer);
-	  controle.className = 'play';
-	} else {
-	  timer = setInterval(trocaBanner, 4000);
-	  controle.className = 'pause';
+	function trocaBanner() {
+		bannerAtual = (bannerAtual + 1) % 2;
+		document.querySelector('.banner img').src = banners[bannerAtual];
 	}
-};
+
+	var timer = setInterval(trocaBanner, 4000);
+	var controle = document.querySelector('.pause');
+
+	controle.onclick = function() {
+		if (controle.className == 'pause') {
+		  clearInterval(timer);
+		  controle.className = 'play';
+		} else {
+		  timer = setInterval(trocaBanner, 4000);
+		  controle.className = 'pause';
+		}
+	};
+}
 
 var botaoMenu = document.querySelector('.nav-btn');
 var navLinks = document.querySelector('.nav-links');
